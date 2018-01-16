acc01fa19d1f1b5696f68dbd1a4c04cbdcb8341d
Alex Spencer <alex@alexspencer.net>
2018-01-16 14:49:26 -0500

    adds more styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -36,19 +36,21 @@ export default class Home extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View>
+        <View style={styles.form}>
           <TextInput
             placeholder='new task'
             value={this.state.todoText}
             style={styles.textInput}
             onChangeText={this._todoTextChange.bind(this)}
           />
-          <Button title='Add' onPress={this.addTask.bind(this)}/>
+          <TouchableOpacity onPress={this.addTask.bind(this)} style={styles.button}>
+            <Text style={styles.buttonText}>Add</Text>
+          </TouchableOpacity>
         </View>
-        <View>
+        <View style={styles.todos}>
           {this.state.todos.map((todo, i) =>
-            <TouchableOpacity key={i} onPress={this.removeTodo.bind(this, i)}>
-              <Text> x | {todo}</Text>
+            <TouchableOpacity key={i} style={styles.todo} onPress={this.removeTodo.bind(this, i)}>
+              <Text style={styles.todoText}> x  {todo}</Text>
             </TouchableOpacity>
           )}
         </View>
@@ -59,15 +61,53 @@ export default class Home extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    margin: 35,
+    margin: 25,
     marginTop: 100,
+  },
+
+  form: {
+    flexDirection: 'row',
+  },
+
+  button: {
+    backgroundColor: 'blue',
+    marginLeft: 5,
+    height: 40,
+    width: 70,
+    borderRadius: 3,
+    justifyContent: 'center',
+    padding: 5
+  },
+
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center'
   },
 
   textInput: {
     height: 40,
-    width: 300,
+    width: 250,
     borderColor: 'gray',
     padding: 5,
     borderWidth: 1,
+    fontSize: 20,
+    borderRadius: 3
+  },
+
+  todo: {
+    borderBottomWidth: .5,
+    borderColor: 'lightgray',
+    marginTop: 10,
+    marginBottom: 10
+  },
+
+  todoText: {
+    fontSize: 20,
+    marginBottom: 2
+  },
+
+  todos: {
+    marginTop: 30,
   }
 })
